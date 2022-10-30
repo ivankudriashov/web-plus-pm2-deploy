2bf1a16eaa8589b79510a8cf20599a4f260a2a11
Ivan Kudriashov <pereyyra@rambler.ru>
2022-10-30 23:23:18 +0300

    Тест

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,9 +13,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
